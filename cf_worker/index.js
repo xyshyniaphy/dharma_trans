de1ca99e4b386b2724e3b250523bca6175d85d0d
lmr <xyshyniaphy@gmail.com>
2025-04-01 14:15:33 +0900

    upd

--- cf_worker/index.js
+++ cf_worker/index.js
@@ -1,23 +1,12 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    const path = url.pathname;
-
-    if (path.startsWith("/access/")) {
-      const key = path.slice("/access/".length);
-      if (!key) {
-        return new Response("No file key provided", { status: 400 });
-      }
-
-      const object = await env.MY_R2_BUCKET.get(key);
+	  
+	const object = await env.MY_R2_BUCKET.get("dic.csv");
       if (object) {
         const content = await object.text();
         return new Response(content);
       } else {
         return new Response("File not found", { status: 404 });
       }
-    } else {
-      return new Response("Invalid path", { status: 404 });
-    }
   },
 };
