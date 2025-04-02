309eade028a90000fdfbbf6909292e274be1439e
xyshy <xyshyniaphy@gmail.com>
2025-04-02 06:27:12 +0000

    remove build error asset

--- viteui/src/api/index.ts
+++ viteui/src/api/index.ts
@@ -3,8 +3,8 @@ const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 
-app.get("*", (c) => {
-  return c.env.ASSETS.fetch(c.req.raw);
-});
+// app.get("*", (c) => {
+//   return c.env.ASSETS.fetch(c.req.raw);
+// });
 
 export default app;
